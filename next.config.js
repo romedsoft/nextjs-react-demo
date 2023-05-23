69e40dd22c28184e9138cf80b8e50003f7190031
Roman Rodriguez <romedsoft@gmaill.com>
2023-05-23 11:39:22 -0600

    adding domain to next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    images:{
+      domains:['test.rrodriguez-software.com']
+    },
     experimental: {
         outputStandalone: true,
       }
